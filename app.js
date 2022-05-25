8dbf7669c674e6cdaa1c22d6aa2f7b648d2400dd
penguin0001 <robynpgreer@gmail.com>
2022-05-25 17:31:06 +0100

    stats

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+// ** GLOBAL VARIABLES ** //
 // maybe have building type objects to store info abt them?
 
 // set up buildings map to track quantities of types of buildings
 let allBuildings = new Map();
-allBuildings.set('house', 0);
+allBuildings.set('houses', 0);
+
+// what it says
+let money = 100;
 
 // this is where we store what building or tool the user has selected
 let building;
 
-// main set up function
+// ********************** //
+
+
+// ** RUN GAME ** //
 setUpAll();
+// ************** //
 
 
 // general start site function
@@ -27,23 +35,18 @@ function setUpAll() {
 
 }
 
-// set up buttons //
-
-// BUILDING buttons
+// set up buttons 
 function setUpBuildingButtons() {
     const buildingButtons = document.querySelectorAll(".building");
     buildingButtons.forEach(buildingButton => {
         buildingButton.style.backgroundColor = 'white';
         buildingButton.style.backgroundImage = `url('images/${buildingButton.id}.png')`;
+        addTranslucentOnHover(buildingButton);
         buildingButton.addEventListener('click', () => {
             building = buildingButton.id;
         });
     });
 }
-
-
-// grid stuff //
-
 
 // make grid
 function makeGrid(size) {
@@ -52,19 +55,9 @@ function makeGrid(size) {
         const div = document.createElement("div");
         div.classList.add("gridElement");
 
-        // border when hovering
-        div.addEventListener("mouseover", () => {
-            div.style.borderStyle = 'solid';
-            div.style.margin = '-4px';
-        });
-
-        // no border when not hovering
-        div.addEventListener("mouseleave", () => {
-            div.style.borderStyle = 'none';
-            div.style.margin = '0px';
-        });
+        addTranslucentOnHover(div);
         
-        // draw
+        // place building
         div.addEventListener('click', () => {
             if (building == 'erase') {
                 div.style.opacity = 0;
@@ -72,15 +65,20 @@ function makeGrid(size) {
 
                 // remove an instance of the building
                 // div.id is the building we want
-                if (allBuildings.get(div.id) <= 0) {
+                if (allBuildings.get(div.id + "s") <= 0) {
                     console.log("error - can't remove more instances of this building");
                 } else {
-                    allBuildings.set(div.id, allBuildings.get(div.id) - 1)
+                    allBuildings.set(div.id + "s", allBuildings.get(div.id + "s")- 1)
                 }
                 
-                // print list of buildings to console
+                // print list of buildings to console -> replace with info card
                 console.log(allBuildings);
 
+                // update money
+                money += 10;
+
+                //update info box
+                updateStats();
             } else {
                 // image
                 div.style.backgroundImage = `url('images/${building}.png')`;
@@ -89,12 +87,16 @@ function makeGrid(size) {
                 div.id = building;
 
                 // add to map of buildings
-                allBuildings.set(div.id, allBuildings.get(div.id) + 1)
+                allBuildings.set(div.id + "s", allBuildings.get(div.id + "s") + 1)
 
-                // print map of buildings to console
+                // print map of buildings to console -> replace with info card
                 console.log(allBuildings);
 
-                // update page?
+                // update money
+                money -= 10;
+
+                // update info box
+                updateStats();
             }
         });
         
@@ -105,7 +107,7 @@ function makeGrid(size) {
     canvas.style.gridTemplateColumns = getAutos(size);
 }
 
-//make guide
+// make guide
 function makeGuide(size) {
     const guide = document.querySelector(".guide");
     for (let i = 0; i < size; i++) {
@@ -117,6 +119,38 @@ function makeGuide(size) {
     // makes it squares in a grid instead of lines!
     guide.style.gridTemplateColumns = getAutos(size);
 }
+
+// adds translucent on hover to an element
+function addTranslucentOnHover(element) {
+    // when hovering
+    element.addEventListener("mouseover", () => {
+        element.style.opacity = '50%';
+    });
+
+    // when not hovering
+    element.addEventListener("mouseleave", () => {
+        element.style.opacity = '100%';
+    });
+}
+
+// update stats box
+function updateStats() {
+    //updateStat("#houses", housesChange);
+    allBuildings.forEach(function (key, value) {
+        updateStat(value, key);
+    });
+
+    updateStat("money", money);
+}
+
+// updates a stat in the stat box
+function updateStat(statName, newValue) {
+    const stat = document.querySelector("#" + statName);
+    //let statValue = parseInt(stat.innerHTML);
+    //statValue += change;
+    stat.innerHTML = newValue;
+}
+
 
 // grid drawing helpers
 
